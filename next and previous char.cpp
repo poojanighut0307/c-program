f6b46140012057da2f9e6f5e50c1cef3f550fc67
poojanighut0307 <142563622+poojanighut0307@users.noreply.github.com>
2023-08-21 19:27:26 +0530

    Add files via upload

---
+++ next and previous char.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("enter character:");
+	scanf("%c",&ch);
+	
+	printf("\n next character =%c",ch+1);
+	printf("\n next character ASCII value =%d",ch+1);
+	
+	printf("\n previous character =%c",ch-1);
+	printf("\n previous character ASCII value =%d",ch-1);
+	
+	
+}
