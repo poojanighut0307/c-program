102ef522d72e26e0cee3a6e645537738b9f85503
poojanighut0307 <142563622+poojanighut0307@users.noreply.github.com>
2023-08-22 19:44:49 +0530

    Add files via upload

---
+++ circle-ac.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	float r,a,c;
+	printf("enter radius= ");
+	scanf("%f",&r);
+	a=3.14*r*r;
+	c=2*3.14*r;
+	printf("area of circle= %f",a);
+	printf("\n circumferance= %f",c);
+}
